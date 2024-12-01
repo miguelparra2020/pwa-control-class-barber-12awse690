c08616bed365b650a92e2684525459ecc33a7e00
@mpaez1 <mpaez@ikatechsolutions.com>
2024-12-01 16:57:51 -0500

    add-changes

--- utils/seoConfig.ts
+++ utils/seoConfig.ts
@@ -7,15 +7,15 @@ import type { ManifestOptions } from "vite-plugin-pwa"
 export const seoConfig = {
 	baseURL: "https://example.com", // Change this to your production URL.
 	description:
-		"Astro PWA Starter is an opionated Astro starter for building robust static websites.", // Change this to be your website's description.
+		"Class barber control", // Change this to be your website's description.
 	type: "website",
 	image: {
 		url: "https://picsum.photos/1200/630", // Change this to your website's thumbnail.
-		alt: "OpenGraph thumbnail description.", // Change this to your website's thumbnail description.
+		alt: "Class barber control", // Change this to your website's thumbnail description.
 		width: 1200,
 		height: 630
 	},
-	siteName: "Class barber", // Change this to your website's name,
+	siteName: "Class barber control", // Change this to your website's name,
 	twitter: {
 		card: "summary_large_image"
 	}
@@ -25,8 +25,8 @@ export const seoConfig = {
  * Defines the configuration for PWA webmanifest.
  */
 export const manifest: Partial<ManifestOptions> = {
-	name: "Class barber", // Change this to your website's name.
-	short_name: "Class barber", // Change this to your website's short name.
+	name: "Class barber control", // Change this to your website's name.
+	short_name: "Class barber control", // Change this to your website's short name.
 	description:
 		"Class barber", // Change this to your websites description.
 	theme_color: "#30E130", // Change this to your primary color.
