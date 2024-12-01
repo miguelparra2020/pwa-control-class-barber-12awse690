54e1b296cd983ed579450bd7fae1ea2ed030cec4
@mpaez1 <mpaez@ikatechsolutions.com>
2024-12-01 07:04:55 -0500

    add-changes

--- src/components/global/InstallButtonPWA.tsx
+++ src/components/global/InstallButtonPWA.tsx
@@ -2,13 +2,10 @@ import React, { useEffect, useState } from "react";
 
 const InstallPWAButton: React.FC = () => {
   const [installPrompt, setInstallPrompt] = useState<Event | null>(null);
-  const [isVisible, setIsVisible] = useState(false);
-
   useEffect(() => {
     const handleBeforeInstallPrompt = (e: Event) => {
       e.preventDefault(); // Evita que el navegador muestre automáticamente el prompt
       setInstallPrompt(e);
-      setIsVisible(true); // Muestra el botón de instalación
     };
 
     window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
@@ -28,17 +25,15 @@ const InstallPWAButton: React.FC = () => {
           console.log("PWA no instalada");
         }
         setInstallPrompt(null);
-        setIsVisible(false); // Oculta el botón tras la acción
       });
     }
   };
 
   return (
-    isVisible && (
       <button onClick={handleInstallClick} style={{ padding: "10px", fontSize: "16px" }}>
         Instalar App
       </button>
-    )
+    
   );
 };
 
