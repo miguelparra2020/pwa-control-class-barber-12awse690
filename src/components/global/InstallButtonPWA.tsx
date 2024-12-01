6558a5f46316527e1da08152c6ca324fc877f16e
@mpaez1 <mpaez@ikatechsolutions.com>
2024-12-01 07:12:39 -0500

    add-changes

--- src/components/global/InstallButtonPWA.tsx
+++ src/components/global/InstallButtonPWA.tsx
@@ -30,10 +30,16 @@ const InstallPWAButton: React.FC = () => {
   };
 
   return (
-      <button onClick={handleInstallClick} style={{ padding: "10px", fontSize: "16px" }}>
-        Instalar App
+    <div className="flex justify-center items-center  ">
+      <button onClick={handleInstallClick} style={{ padding: "10px", fontSize: "16px" }} className="flex justify-center items-center flex-row bg-gray-500 text-white rounded w-[90%] mb-10">
+        Instalar App &nbsp;
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-in-down" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1z"/>
+        <path fill-rule="evenodd" d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+        </svg>
       </button>
-    
+      
+      </div>
   );
 };
 
